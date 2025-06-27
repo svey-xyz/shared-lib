06f1432d2e651a16f11c4d764efd5f5f6a2628d2
svey-xyz <x@svey.xyz>
2025-06-27 14:57:59 -0400

    Modulerize string transforms

--- index.ts
+++ index.ts
@@ -1,4 +1,1 @@
-export { capitalize } from "./src/capitalize";
-export { camelCaseToWords } from "./src/camelCaseToWords";
-export { pluralize } from "./src/pluralize";
-export { slugify as slugifyWithOptions } from "./src/slugify";
+export { StringTransforms } from "./src/stringTransforms";
---
+++ src/stringTransforms/index.ts
@@ -0,0 +1,11 @@
+import { camelCaseToWords } from "./camelCaseToWords";
+import { capitalize } from "./capitalize";
+import { pluralize } from "./pluralize";
+import { slugify } from "./slugify";
+
+export const StringTransforms = {
+	camelCaseToWords,
+	capitalize,
+	pluralize,
+	slugify,
+};
