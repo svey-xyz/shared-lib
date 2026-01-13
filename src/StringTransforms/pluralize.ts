499a4ebc1869299ee60d507b17f6f30340ca950b
svey <x@svey.xyz>
2026-01-13 10:52:25 -0500

    Singularize

--- src/StringTransforms/pluralize.ts
+++ src/StringTransforms/pluralize.ts
@@ -1,5 +1,9 @@
-import { plural } from 'pluralize'
+import { plural, singular } from 'pluralize'
 
 export const pluralize = (string: string) => {
+	return plural(string)
+}
+
+export const singularize = (string: string) => {
 	return plural(string)
 }
